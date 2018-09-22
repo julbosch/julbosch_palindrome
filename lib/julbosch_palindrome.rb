29ed81eef5297718930b41839b60a7c278ee7682
Julius Bosch <juliusbosch89@gmail.com>
2018-09-21 20:41:57 -0700

    Finish Palindrome method

--- lib/julbosch_palindrome.rb
+++ lib/julbosch_palindrome.rb
@@ -1,5 +1,17 @@
-require "julbosch_palindrome/version"
+# require "julbosch_palindrome/version"
 
-module JulboschPalindrome
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    return processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      return self.scan(/[a-z]/i).join.downcase
+    end
+
 end
